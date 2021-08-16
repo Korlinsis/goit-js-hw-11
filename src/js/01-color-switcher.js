b0c8758e0c097f283f1cc31c055fffefee63d3a7
Ivan Kiziuk <ivanmnksos@gmail.com>
2021-08-16 20:53:35 +0300

    Colorswicher and Timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+
+let timer = null;
+
+function onStartButtonClick() {
+  refs.startButton.setAttribute('disabled', '');
+  refs.startButton.classList.add('is-active');
+
+  timer = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  clearInterval(timer);
+  refs.startButton.removeAttribute('disabled');
+  refs.startButton.classList.remove('is-active');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,75 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from "notiflix";
+
+const refs = {
+  dateInput:document.querySelector ('#datetime-picker'),
+  startButton: document.querySelector('[data-start]'),
+  daysField: document.querySelector('[data-days]'),
+  hoursField: document.querySelector('[data-hours]'),
+  minutesField: document.querySelector('[data-minutes]'),
+  secondsField: document.querySelector('[data-seconds]'),
+};
+
+let selectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() > Date.now()) {
+        refs.startButton.removeAttribute('disabled');
+        Notiflix.Notify.success('Щоб розпочати відлік часу, натисність кнопку "START"!');
+        selectedDate = selectedDates[0].getTime();
+        console.log(selectedDate);
+        return selectedDate;
+    } else {
+        refs.startButton.setAttribute('disabled', '');
+        Notiflix.Notify.failure('Виберіть дату, яка ще не настала!');
+    }
+  },
+};
+
+flatpickr(refs.dateInput, options)
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+
+function onStartButtonClick() {
+    let IntervalId = null;
+
+    IntervalId = setInterval(() => {
+        const currentDate = Date.now();
+        const DeltaDate = selectedDate - currentDate;
+        const { days, hours, minutes, seconds } = convertMs(DeltaDate);
+        refs.secondsField.textContent = seconds;
+        refs.minutesField.textContent = minutes;
+        refs.hoursField.textContent = hours;
+        refs.daysField.textContent = days;
+    }, 1000);
+
+    // if 
+
+    // function stop() {
+    //     clearInterval(IntervalId);
+    // }
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
