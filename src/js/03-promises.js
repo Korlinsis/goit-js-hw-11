e7b553304c897bf84dced8fca91c4d4fa56515af
Ivan Kiziuk <ivanmnksos@gmail.com>
2021-08-18 17:25:31 +0300

    start task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+const submitButton = document.querySelector('[date-submit]');
+
+submitButton.addEventListener("submit", createPromise);
+
+function createPromise(e, position, delay) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = e.currentTarget;
+  
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve
+      }
+      reject
+    }, delay);
+  });
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
