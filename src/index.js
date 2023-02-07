fcb5198e3f2f7633c9309ceb06019c8eda58a422
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-02-07 23:46:30 +0200

    Add first style

--- src/index.js
+++ src/index.js
@@ -7,3 +7,23 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const API_KEY = '33457502-2f0900523bf20d938f40b8c6d';
 const BASE_URL = 'https://pixabay.com/api/';
+
+function createMarkup () {
+    return `<div class="photo-card">
+        <img src="" alt="" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+            <b>Likes</b>
+            </p>
+            <p class="info-item">
+            <b>Views</b>
+            </p>
+            <p class="info-item">
+            <b>Comments</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads</b>
+            </p>
+        </div>
+    </div>`;
+}
