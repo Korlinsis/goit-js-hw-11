62e3d1d17ea211aa819501d6eef796c0e5c42c8a
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-02-07 22:51:34 +0200

    Start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/styles.css';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const API_KEY = '33457502-2f0900523bf20d938f40b8c6d';
+const BASE_URL = 'https://pixabay.com/api/';
