ba16363b12717e15a38bd77550db46d9a7dbcc8b
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-02-11 21:43:46 +0200

    Work version without async/await

--- src/commponents/API.js
+++ src/commponents/API.js
@@ -4,10 +4,9 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '33457502-2f0900523bf20d938f40b8c6d';
 const SEARCH_PARAMETERS = '&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
 
-export async function getPictures(request) {
+export async function getPictures(request, page) {
     try {
-      const {data} = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${request}${SEARCH_PARAMETERS}`);
-      console.log(data);
+      const {data} = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${request}${SEARCH_PARAMETERS}&page=${page}`);
       return data;
     } catch (error) {
       console.error(error);
--- src/index.js
+++ src/index.js
@@ -8,32 +8,75 @@ import { getPictures } from './commponents/API';
 const searchForm = document.querySelector('.search-form');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
+let page = 0;
+let request = '';
+let lightbox = null;
 
 searchForm.addEventListener('submit', showPictures);
 loadMoreButton.addEventListener('click', loadMore);
-
-
+loadMoreButton.classList.add('is-hidden');
 
 function showPictures(e) {
     e.preventDefault();
-    const {elements: { searchQuery }} = e.currentTarget;
-    if (searchQuery.value.trim() === '') return;
+    galleryContainer.innerHTML = '';
+    loadMoreButton.classList.add('is-hidden');
 
-    getPictures(searchQuery.value.trim())
+    const {elements: { searchQuery }} = e.currentTarget;
+    request = searchQuery.value.trim();
+    page = 1;
+
+    if (request === '') return;
+
+    getPictures(request, page)
     .then(({hits, totalHits}) => {
         if (hits.length === 0) {
-        return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            throw new Error ('Not found.');
         } else {
-        Notify.success(`Hooray! We found ${totalHits} images.`);
-        return hits;
-    }})
+            loadMoreButton.classList.remove('is-hidden');
+            if (hits.length !== 40) loadMoreButton.classList.add('is-hidden');
+            Notify.success(`Hooray! We found ${totalHits} images.`);
+            return hits;
+        }
+    })
     .then(hits => {
         galleryContainer.innerHTML = hits.map(hit => createMarkup(hit)).join('');
-        new SimpleLightbox('.gallery a');
-    })    
+        lightbox = new SimpleLightbox('.gallery a');
+        page += 1;
+    })
+    .catch(requestError);   
 }
 
 function loadMore() {
+    getPictures(request, page)
+    .then (({hits}) => {
+        if (hits.length !== 40) {
+            Notify.info(`We're sorry, but you've reached the end of search results.`);
+            loadMoreButton.classList.add('is-hidden');
+        }
+        galleryContainer.insertAdjacentHTML('beforeend', hits.map(hit => createMarkup(hit)).join(''));
+        lightbox.refresh();
+        smoothScroll();
+        page += 1;
+    })
+    .catch(limitedAccess);
+}
 
+function requestError() {
+    return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+
+function limitedAccess() {
+    return Notify.warning('Sorry, you are using the free version of Pixabay. Approve your account for full API access and get more pictures.');
+}
+
+function smoothScroll() {
+    const { height: cardHeight } = document
+    .querySelector(".gallery")
+    .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+    });
 }
 
